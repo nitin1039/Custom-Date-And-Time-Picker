20946a681b4ba1f0cc107f2209de3be852362d75
nitin1039 <nitinprajapati1039@gmail.com>
2023-03-28 15:10:11 +0530

    third commit

--- App.js
+++ App.js
@@ -6,6 +6,10 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 export default function App() {
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
   const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+  const [selectedDate,setSelectedDate]=useState("Select Date")
+  const [selectedTime, setSelectedTime]=useState("Select Time")
+
+
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -17,6 +21,11 @@ export default function App() {
 
   const handleDateConfirm = (date) => {
     console.warn("A date has been picked: ", date);
+    const dt=new Date(date);
+    const x=dt.toISOString().split("T");
+    const x1=x[0].split("-");
+    // console.log()
+    setSelectedDate(x1[2]+"/"+x1[1]+"/"+x1[0])
     hideDatePicker();
   };
 
@@ -30,8 +39,12 @@ export default function App() {
   
   };
 
-  const handleTimeConfirm = (time) => {
-    console.warn("A time has been picked: ", time);
+  const handleTimeConfirm = (date) => {
+    console.warn("A time has been picked: ",date);
+    const dt=new Date(date);
+    const x=dt.toLocaleTimeString();
+    setSelectedTime(x)
+    // console.log(x)
     hideTimePicker();
   };
 
@@ -51,7 +64,7 @@ export default function App() {
           showDatePicker();
         }}
       >
-        <Text>Select Date</Text>
+        <Text>{selectedDate}</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
@@ -69,7 +82,7 @@ export default function App() {
           showTimePicker();
         }}
       >
-        <Text>Select Time</Text>
+        <Text>{selectedTime}</Text>
       </TouchableOpacity>
 
       <DateTimePickerModal
